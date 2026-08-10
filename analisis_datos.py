14b7d5ce763413e6384d00d6708e31f36134a832
Dami0921 <damiancorreatusarma248@gmail.com>
2026-08-10 17:08:02 -0500

    Agregado script analisis_datos.py con condicionales

---
+++ analisis_datos.py
@@ -0,0 +1,6 @@
+temperatura = int(input("¿cual es la tempratura actual?"))
+ciudad = input("¿en que ciudad estas'")
+
+if temperatura > 35:
+    print ("hace calor en", ciudad)
+else: print ("hace frio en" , ciudad)
